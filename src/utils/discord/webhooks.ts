bf7b1b921a41b1689c3da469082fda2b11e37b8e
Mameng <mamengsky52@gmail.com>
2024-12-04 23:11:17 +0700

    XClubV2
    Change webhook

--- src/utils/discord/webhooks.ts
+++ src/utils/discord/webhooks.ts
@@ -1,4 +1,4 @@
 export const webhookUrls = {
-  default: 'https://discord.com/api/webhooks/1313599353469796352/__DENL67b8xplxpJUDlF8HHkw1VbqgthTf8iRAUqQgEF7oDVl10ldOCjg_HUE1pyfTm-',
-  officeMoney: 'https://discord.com/api/webhooks/1313627934434791487/BImWiUmsFwJ6VR-IEinCCD34Wc-avaCFaLf6Hh-DuJx4LQzxN6A5xZFbJpzQgfehEfra'
+  default: 'https://discord.com/api/webhooks/1313896680898428999/dtTm5XR05sVkHOtR7R5CiHmYIMdSxs5Lv7iYwCpvWWBtCa3I36mWQeYUhKrBYKu5ua5C',
+  officeMoney: 'https://discord.com/api/webhooks/1313897019416379452/ysAiuOE3EZu_oZkrbDLlwA_gc8l-9hcHd1xExgSoQfT9Q_bfEIVUrJp5I04aghr2KmXA'
 };
